69595ac87509f6dba0d84da4151156ef1b05e0cb
organisedtoast <68993412+organisedtoast@users.noreply.github.com>
2026-02-03 15:24:33 +1100

    Created external buttonscript JS file for a cleaner look.

---
+++ module1/LabExercises/exercise3/buttonscript.js
@@ -0,0 +1,11 @@
+function chooseteam1() {
+  let dreamPlayer = document.getElementById("dreamplayer1").value;
+  document.getElementById("heading1").textContent = "98/99 squad PLUS " + dreamPlayer + " ACTIVATED";
+  document.getElementById("column1").style.background = "gold";
+}
+
+function chooseteam2() {
+  let dreamPlayer = document.getElementById("dreamplayer2").value;
+    document.getElementById("heading2").textContent = "25/26 squad PLUS " + dreamPlayer + " ACTIVATED";
+  document.getElementById("column2").style.background = "lightblue";
+}
