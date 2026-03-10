d7f8777105a1dc401fb75c6ec89ca2da724dc7c2
organisedtoast <68993412+organisedtoast@users.noreply.github.com>
2026-03-10 14:05:22 +1100

    Commit institute-of-data-labs\module5\exercise4

--- module5/exercise4/routes/friendRoutes.js
+++ module5/exercise4/routes/friendRoutes.js
@@ -2,11 +2,6 @@ const express = require("express");
 const router = express.Router();
 const friends = require('../models/friends')
 
-
-// TODO - #1: Add support to the 'filter' endpoint for a new query parameter 'letter' which filters friends by starting letter
-
-// TODO - #2: Modify the 'info' route to only return the user-agent, content-type and accept header data
-
 // TODO - #3: Modify the dynamic GET route to return a single friend object matching the dynamic 'id' request parameter
 
 // TODO - #4: Complete the PUT route which will update data for an existing friend
@@ -21,6 +16,8 @@ router.get('/', (req, res) => {
 
 // filter endpoint, gets friends matching the gender from 'gender' query parameter ie. /friends/filter?gender=male
 // 1. Add support to also filter by a starting 'letter' query parameter ie. /friends/filter?letter=R
+
+/*
 router.get('/filter', (req, res) => {
     console.log(req.query)
     let filterGender = req.query.gender;
@@ -38,16 +35,59 @@ router.get('/filter', (req, res) => {
         res.status(404).json({error: "No friends matching gender "+filterGender})
     }  
 })
+*/
+
+
+
+// QUESTION 1: Add support to the 'filter' endpoint for a new query parameter 'letter' which filters friends by starting letter of their name
+
+router.get('/filter', (req, res) => {
+    console.log(req.query)
+    let filterGender = req.query.gender;
+    let filterLetter = req.query.letter;
+    let matchingFriends = [...friends];
+
+    if (filterGender) {
+        matchingFriends = matchingFriends.filter(friend => friend.gender == filterGender);
+    }
+
+    if (filterLetter) {
+        matchingFriends = matchingFriends.filter(friend => friend.name.startsWith(filterLetter));
+    }
+
+    if (matchingFriends.length > 0) {
+        // return valid data when the gender matches 
+        res.status(200).json(matchingFriends)
+    } else {
+        // and an error response when there are no matches
+        res.status(404).json({error: "No friends matching the given criteria"})
+    }  
+})  
+
 
 // 2. Get information about this request from the headers
+
+/*
 router.get('/info', (req, res) => {
     console.log(req.headers)
 
     // Modify this response to just return info on the user-agent, content-type and accept headers
     res.json(req.headers)  
 })
+*/
 
-// 3. Dynamic request param endpoint - get the friend matching the specific ID ie. /friends/3
+// QUESTION 2: Modify the 'info' route to only return the user-agent, content-type and accept header data
+router.get('/info', (req, res) => {
+    const { 'user-agent': userAgent, 'content-type': contentType, accept } = req.headers;
+    res.json({ userAgent, contentType, accept });
+});
+
+
+
+
+
+
+// QUESTION 3: Dynamic request param endpoint - get the friend matching the specific ID ie. /friends/3
 router.get('/:id', (req, res) => {
     console.log(req.params)
     let friendId = req.params.id; // 'id' here will be a value matching anything after the / in the request path
