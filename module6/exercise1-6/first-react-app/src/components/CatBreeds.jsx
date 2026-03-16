d45b927cfa57bab5c281072c2af8c1d554dcc310
organisedtoast <68993412+organisedtoast@users.noreply.github.com>
2026-03-16 13:59:04 +1100

    Added filterRealCats function and associated button in CatBreeds.jsx

--- module6/exercise1-6/first-react-app/src/components/CatBreeds.jsx
+++ module6/exercise1-6/first-react-app/src/components/CatBreeds.jsx
@@ -6,12 +6,13 @@ import SingleCat from './SingleCat'
 // the name of this variable is "cats" and it will hold an array of cat breed objects
 // each object represents a cat breed with its name, image URL and latin name
 const cats = [
-{ id: 1, name: 'Bengal', latinName: 'felis bengalensis', imageUrl: 'https://image.petmd.com/files/inline-images/bengal-cat.jpeg?VersionId=X0xkDftr_klFvUhQpLarkxvJBbnUAd01' },
-{ id: 2, name: 'Siamese', latinName: 'felis catus siamensis', imageUrl: 'https://www.purina.co.uk/sites/default/files/styles/square_medium_440x440/public/2022-06/Siamese%201.jpg?h=c17eaee4&itok=BRsemy6v' },
-{ id: 3, name: 'Scottish Fold', latinName: 'felis catus scottishfoldus', imageUrl: 'https://purebredkitties.com/cdn/shop/articles/scottish-fold-ear-health-caring-both-folded-standard-varieties.webp?v=1755326499' },
-{ id: 4, name: 'Abyssinian', latinName: 'felis catus abyssinicus', imageUrl: 'https://cdn-ilecccn.nitrocdn.com/RanGABiTadbcbUtojmhXDdudFjKCdOMD/assets/images/optimized/rev-ebd86f6/www.worldsbestcatlitter.com/wp-content/uploads/2024/12/abyssinian-cats-blog_main-img.jpg' },
-{ id: 5, name: 'Burmese', latinName: 'felis catus burmensis', imageUrl: 'https://www.burgesspetcare.com/wp-content/uploads/2025/12/All-about-Burmese-cats-.jpg' },
-{ id: 6, name: 'Doraemon', latinName: 'automata felis caerulea', imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROqjwu6euZ7Y-4S8n_ERj8p9DciT5oCoZvb4iKHqxp4vlzaRe3cEUXQ7-iEhowf3WGyyUuLhxaP4Nh_Hes-oaQ1LP_toPEGqhcZjNyYs63&s=10' },
+{ id: 1, name: 'Doraemon', latinName: 'doraemonus mechanicus', imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROqjwu6euZ7Y-4S8n_ERj8p9DciT5oCoZvb4iKHqxp4vlzaRe3cEUXQ7-iEhowf3WGyyUuLhxaP4Nh_Hes-oaQ1LP_toPEGqhcZjNyYs63&s=10' },
+{ id: 2, name: 'Bengal', latinName: 'felis catus bengalensis', imageUrl: 'https://image.petmd.com/files/inline-images/bengal-cat.jpeg?VersionId=X0xkDftr_klFvUhQpLarkxvJBbnUAd01' },
+{ id: 3, name: 'Siamese', latinName: 'felis catus siamensis', imageUrl: 'https://www.purina.co.uk/sites/default/files/styles/square_medium_440x440/public/2022-06/Siamese%201.jpg?h=c17eaee4&itok=BRsemy6v' },
+{ id: 4, name: 'Scottish Fold', latinName: 'felis catus scottishfoldus', imageUrl: 'https://purebredkitties.com/cdn/shop/articles/scottish-fold-ear-health-caring-both-folded-standard-varieties.webp?v=1755326499' },
+{ id: 5, name: 'Abyssinian', latinName: 'felis catus abyssinicus', imageUrl: 'https://cdn-ilecccn.nitrocdn.com/RanGABiTadbcbUtojmhXDdudFjKCdOMD/assets/images/optimized/rev-ebd86f6/www.worldsbestcatlitter.com/wp-content/uploads/2024/12/abyssinian-cats-blog_main-img.jpg' },
+{ id: 6, name: 'Burmese', latinName: 'felis catus burmensis', imageUrl: 'https://www.burgesspetcare.com/wp-content/uploads/2025/12/All-about-Burmese-cats-.jpg' },
+
 ]
 
 
@@ -30,22 +31,36 @@ function CatBreeds() {
     // it uses the localeCompare method to compare the name properties of a and b and return a negative, zero or positive value depending on their order
     const sorted = [...sortedCats].sort((a, b) => a.name.localeCompare(b.name))
 
+
+    // VIEW BY ASCENDING ALPHABETICAL ORDER
     // call the setSortedCats function with the sorted array to update the state and trigger a re-render of the component
     // this will cause the list of cat breeds to be displayed in alphabetical order by their name
     setSortedCats(sorted)
   } // close the sortByName function
 
+
+  // REVERSE THE VIEW ORDER
   // declare a function called reverseSort which will reverse the sortedCats array
   const reverseSort = () => {
     const reversed = [...sortedCats].reverse()
     setSortedCats(reversed)
   } // close the reverseSort function
 
+
+  // RESET VIEW TO ORIGINAL ORDER
   // declare a function called resetSort which will reset the sortedCats array back to the original cats array
   const resetSort = () => {
     setSortedCats(cats)
   } // close the resetSort function
 
+  // REAL CATS ONLY FILTER
+  // declare a function called filterRealCats which will filter the sortedCats array to show only cats with "felis" in their latinName
+  const filterRealCats = () => {
+    const realCats = sortedCats.filter(cat => cat.latinName.includes('felis'))
+    setSortedCats(realCats)
+  } // close the filterRealCats function
+
+  // DELETE CAT ENTRY
   // declare a function called deleteCat which will take an id as a parameter and filter the sortedCats array to remove the cat breed object with that id
   // this function will be called when the delete button is clicked in the SingleCat component,
   // and it will update the state to reflect the deletion of the cat breed from the list  
@@ -63,6 +78,7 @@ function CatBreeds() {
       <button onClick={sortByName}>Sort by Name</button>
       <button onClick={reverseSort}>Reverse Sort</button>
       <button onClick={resetSort}>Reset</button>
+      <button onClick={filterRealCats}>Real Cats Only</button>
       <ul>
 
         {/*use the map method to iterate over the sortedCats array and render a SingleCat component for each cat breed object */}
