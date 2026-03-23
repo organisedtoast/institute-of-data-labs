37b994db5547c48c561f6d52b14dc1447c877526
organisedtoast <68993412+organisedtoast@users.noreply.github.com>
2026-03-23 12:05:48 +1100

    placeholder institute-of-data-labs\module7\first-next-app

---
+++ module7/first-next-app/src/app/about/page.js
@@ -0,0 +1,17 @@
+// This code defines a React functional component called `About`.
+
+// It returns a JSX structure that includes a heading and a paragraph describing the about page of the Next.js application. 
+
+// The component is then exported as the default export of the module, allowing it to be imported and used in other parts of the application.
+
+function About () {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>This is the about page of our Next.js application.</p>
+        </div>
+    );
+}
+
+export default About;
+
---
+++ module7/first-next-app/src/app/contact/page.js
@@ -0,0 +1,10 @@
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>You can reach us at 999999999 or email us at asdf@gmail.com.</p>
+    </div>
+  );
+}
+
+export default Contact;
