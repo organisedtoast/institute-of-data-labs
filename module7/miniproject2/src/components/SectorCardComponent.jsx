e8bce5c3d0369fd53dd0d65ebde39302d0129022
organisedtoast <68993412+organisedtoast@users.noreply.github.com>
2026-03-26 01:14:16 +1100

    Added chart card and chart placeholder

---
+++ module7/miniproject2/src/components/SectorCardComponent.jsx
@@ -0,0 +1,46 @@
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+// Import the SectorChart component to display inside the card
+import SectorChart from './SectorChart';
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
+export default function SectorCardComponent() {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      
+      {/* Render the SectorChart component inside the card */}
+      <SectorChart />
+      
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
---
+++ module7/miniproject2/src/components/SectorChart.jsx
@@ -0,0 +1,39 @@
+import { LineChart } from '@mui/x-charts/LineChart';
+import { XAxis } from '@mui/x-charts/models';
+import {
+  dateAxisFormatter,
+  percentageFormatter,
+  usUnemploymentRate,
+} from '../dataset/usUnemploymentRate';
+
+const xAxis: XAxis<'time'>[] = [
+  {
+    dataKey: 'date',
+    scaleType: 'time',
+    valueFormatter: dateAxisFormatter,
+  },
+];
+const yAxis = [
+  {
+    valueFormatter: percentageFormatter,
+  },
+];
+const series = [
+  {
+    dataKey: 'rate',
+    showMark: false,
+    valueFormatter: percentageFormatter,
+  },
+];
+export default function SectorChart() {
+  return (
+    <LineChart
+      dataset={usUnemploymentRate}
+      xAxis={xAxis}
+      yAxis={yAxis}
+      series={series}
+      height={300}
+      grid={{ vertical: true, horizontal: true }}
+    />
+  );
+}
