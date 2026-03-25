a7c67857e89eedd00fab98aaa0818718fe9eb016
organisedtoast <68993412+organisedtoast@users.noreply.github.com>
2026-03-25 14:22:56 +1100

    Update

--- module7/exercise12345/components/Home.jsx
+++ module7/exercise12345/components/Home.jsx
@@ -8,7 +8,7 @@ export default function Homepage() {
     <div className="Homepage">
       <h1>Home</h1>
 
-<p>Dear Mirza/ Gareth. I have abandoned this exercise and will move on to Mini Project 2. Reason is I don't want to delete my existing CSS for these new MUI components which are styled with weird dependency files (which go into src\themes). I will do Mini Project 2 with ONLY MUI components (no CSS) - it will have MUI cards, MUI form, MUI App Bar, data API so will definitely cover for this exercise and hopefully won't look like complete dog shit.</p>
+<p>Dear Mirza/ Gareth. I have abandoned this exercise and will move on to Mini Project 2. Reason is I don't want to let go of my existing CSS in favour of these new MUI components which are styled through weird dependency files (which go into src\themes). I will do Mini Project 2 with ONLY MUI components (no CSS) - it will have MUI cards, MUI form, MUI App Bar, data API so will definitely cover for this exercise and hopefully won't look like complete dog shit.</p>
 
   <BitcoinRates />
 
